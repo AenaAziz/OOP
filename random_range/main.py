45e3c6d2d87f31ae1efb59e8d43974348e14b674
Aena Aziz <aenaaziz1246@gmail.com>
2025-09-21 22:40:09 +0500

    Add files via upload

---
+++ random_range/main.py
@@ -0,0 +1,3 @@
+from random_range_composition import RandomElement 
+e = RandomElement([5, 10, 15, 20])
+print(e.choice())  
---
+++ random_range/random_range_composition.py
@@ -0,0 +1,22 @@
+import random
+
+class RandomElement:
+    #composition
+    def __init__(self, seq):
+        if isinstance(seq, RandomElement): self._list = list(seq._list)
+        elif isinstance(seq, (list, tuple, str)): 
+           self._list = list(seq) 
+           self._list_values_validation()
+        else: raise TypeError("invalid seqeuence type")
+
+    def _list_values_validation(self):
+        for i, item in enumerate(self._list):
+         if not isinstance(item, (int, str)): raise ValueError("seqeuece must contain digits")
+         if isinstance(item ,str):
+            if not item.isdigit(): raise ValueError("seqeuece must contain digits")
+            self._list[i] = int(item)  
+
+    def choice(self):
+       "pythonic"
+       result = self._list[random.randint(0, len(self._list)-1)]
+       return result
