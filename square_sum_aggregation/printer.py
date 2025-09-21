f24a100228902a4090bcda0decbadd5ff1d733ba
Aena Aziz <aenaaziz1246@gmail.com>
2025-09-21 22:32:56 +0500

    Delete square_sum_aggregation directory

--- square_sum_aggregation/printer.py
+++
@@ -1,8 +0,0 @@
-from square_sum import SquareSumCalculator
-class Printer:
-    def __init__(self, data:SquareSumCalculator):
-        self._data = data
-
-    def display(self):
-        print("Square sum:", self._data)
-    
