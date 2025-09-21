61929410800f2372f470f43a50a760665fb1cbd5
Aena Aziz <aenaaziz1246@gmail.com>
2025-09-21 22:33:12 +0500

    Delete square_sum directory

--- square_sum/square_sum_association/printer.py
+++
@@ -1,8 +0,0 @@
-from square_sum import SquareSum
-
-class Printer:
-    def display(self, sum:SquareSum, n:int):
-        print("Sum of Squares:", sum.sum_square(n))
-
-    def traditional_display(self, sum:SquareSum, n:int):
-        print("Sum of Squares traditionally:", sum.tradi_sum_square(n))  
--- square_sum/square_sum_compositon/main.py
+++
@@ -1,8 +0,0 @@
-from printer import  Printer
-
-if __name__ == "__main__":
-    printer1 = Printer(8)
-    printer1.display()
-
-    printer1.traditional_result_display()
-
